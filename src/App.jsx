5aa4a8a1f275d513104784a08b75d8479bb5baff
Valtteri <valtteri.isomaki@adworks.fi>
2025-11-24 15:36:51 +0200

    centeringg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 import AddDrink from './components/AddDrink';
 import Status from './components/Status';
 import CaffeineChart from './components/CaffeineChart';
